9d68870f3014c3e85a7f4a1af2bce93789d855de
HU <18931177736@189.cn>
2020-05-21 20:14:02 +0800

    flask学习之表单

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template,flash,redirect
+from flask import render_template,flash,redirect,url_for
 from .forms import LoginForm
 
 @app.route('/')
@@ -20,12 +20,11 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html',
-        title = 'Sign In',
-        form = form)
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
