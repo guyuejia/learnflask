1e8bd2ac474b89671c3c5e32be20290caac8e61a
HU <18931177736@189.cn>
2020-05-21 22:54:58 +0800

    flask学习之登录和注册

--- app/models.py
+++ app/models.py
@@ -10,8 +10,11 @@
 
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -21,6 +24,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    #利用hash存储用户的密码
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,3 +39,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
