a6e36c446fe104f02a27a5c78043772949b172bd
HU <18931177736@189.cn>
2020-05-30 17:38:37 +0800

    flask学习之实现分页功能

--- config.py
+++ config.py
@@ -16,3 +16,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['hushishuai.fly@hotmail.com']
+    POSTS_PER_PAGE = 3
